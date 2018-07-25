682078048f67dbc1fb8c3ce91ddcf5c615865cb0
Daniele Zurico <daniele.zurico@blackswan.com>
2018-07-25 20:20:07 +0100

    remove class not needed

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -11,31 +11,29 @@ import { styles } from './dashboard.style';
 import { SideBar, MenuBar, Content } from '../../components';
 import routes from '../../routes';
 
-class Dashboard extends React.Component {
-  render() {
-    const { classes, openSidebar, closeSidebar } = this.props;
-
-    return (
-      <div className={classes.root}>
-        <MenuBar open={this.props.sideBar} openSidebar={openSidebar} />
-        <SideBar
-          open={this.props.sideBar}
-          closeSidebar={closeSidebar}
-          items={mailFolderListItems}
-        />
-        <Content routes={routes} />
-      </div>
-    );
-  }
-}
+const _Dashboard = ({ classes, theme, openSidebar, closeSidebar, sideBar }) => {
+  return (
+    <div className={classes.root}>
+      <MenuBar open={sideBar} openSidebar={openSidebar} />
+      <SideBar
+        open={sideBar}
+        closeSidebar={closeSidebar}
+        items={mailFolderListItems}
+      />
+      <Content routes={routes} />
+    </div>
+  );
+};
 
 const mapStateToProps = state => ({
   sideBar: state.layout.sideBar,
 });
 
-export default withRouter(
+const Dashboard = withRouter(
   connect(
     mapStateToProps,
     { openSidebar, closeSidebar }
-  )(withStyles(styles, { withTheme: true })(Dashboard))
+  )(withStyles(styles, { withTheme: true })(_Dashboard))
 );
+
+export default Dashboard;
